a2487129392dfd4468c0022db64fe0feb7c96ee7
Christopher Siu <christopher@siu.sh>
2024-06-26 09:55:19 -0700

    Added examples of unit tests with coverage.

--- 06.26-correctness/fibonacci.py
+++ 06.26-correctness/fibonacci.py
@@ -8,20 +8,12 @@ def fibonacci(n):
     :param n: A non-negative integer
     :return: The n'th Fibonacci number
     """
-    current = previous = None
-
-    for i in range(n + 1):
-        if i == 0:
-            current = 0
-        elif i == 1:
-            previous = current
-            current = 1
-        else:
-            temp = previous
-            previous = current
-            current = current + temp
-
-    return current
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 if __name__ == "__main__":
--- 06.26-correctness/fibonacci_tests.py
+++ 06.26-correctness/fibonacci_tests.py
@@ -6,6 +6,26 @@ class TestFibonacci(unittest.TestCase):
     def test01(self):
         self.assertEqual(fib.fibonacci(0), 0)
 
+    # If all we have is the above test, the trivial function that simply
+    #  always returns 0 would pass all of our tests.
+
+    def test02(self):
+        self.assertEqual(fib.fibonacci(1), 1)
+
+    # If all we had were the above two tests, we would not have 100% coverage;
+    #  we would never have tested the recursive case.
+
+    def test03(self):
+        self.assertEqual(fib.fibonacci(4), 3)
+
+    # Note that even with 100% coverage, we can only be reasonably confident
+    #  in the code we wrote. Coverage cannot tell us if we needed to write more
+    #  code. For example, if we wanted to handle negative values, the code we
+    #  wrote is covered and correct, but we would need a fourth case:
+
+    # def test04(self):
+    #    self.assertIsNone(fib.fibonacci(-1))
+
 
 if __name__ == "__main__":
     unittest.main()
