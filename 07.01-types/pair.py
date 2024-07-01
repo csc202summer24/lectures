6390102f04266f137c977a458075c3e3576c8fe6
Christopher Siu <christopher@siu.sh>
2024-07-01 09:49:22 -0700

    Added example of classes and ADTs.

--- 07.01-types/pair.py
+++ 07.01-types/pair.py
@@ -1,6 +1,36 @@
 class Pair:
     """ A pair of elements """
 
+    # This creates a new type, "Pair", but because this is a new type, the
+    #  Python interpreter has no idea how to operate on it.
+
     def __init__(self, first, second):
+
+        # This tells the interpreter how to create a value of type "Pair".
+
         self.first = first
         self.second = second
+
+    def __eq__(self, other):
+
+        # This tells the interpreter when two values of type "Pair" are equal.
+
+        return (type(other) == type(self)
+                and other.first == self.first
+                and other.second == self.second)
+
+    def __repr__(self):
+
+        # This tells the interpreter how to print out a value of type "Pair".
+
+        return "Pair(%r, %r)" % (self.first, self.second)
+
+
+def swap(pair):
+    """
+    Swap the elements of a pair.
+
+    :param pair: A pair of elements
+    :return: A new pair with swapped elements.
+    """
+    return Pair(pair.second, pair.first)
