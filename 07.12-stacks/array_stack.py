7f1f47073f4dcddf7a7f3828706ce98591fb43c7
Christopher Siu <christopher@siu.sh>
2024-07-12 09:59:36 -0700

    Added pseudocode for stacks.

--- 07.12-stacks/array_stack.py
+++ 07.12-stacks/array_stack.py
@@ -11,12 +11,27 @@ class Stack:
 
 
 def push(stack, value):
+    # NOTE: Stacks are a special case of lists: they are lists that are only
+    #       accessible at one terminus. In an array list, the end can be added
+    #       or removed in O(1) time, whereas the beginning would require
+    #       O(n) time, thus, the end ought to be the top-of-stack.
+    #
+    # If the size is equal to the capacity, then:
+    #     Double the capacity.
+    #     Create a new array of that new capacity.
+    #     Copy each element of the old array into the new array.
+    #     Set the backing array to the new array.
+    # Set the element at index "size" in the backing array to the given value.
+    # Increment the size.
     pass
 
 
 def pop(stack):
+    # Decrement the size.
+    # Return the element at index "size" in the backing array.
     pass
 
 
 def peek(stack):
+    # Return the element at index "size - 1" in the backing array.
     pass
--- 07.12-stacks/linked_stack.py
+++ 07.12-stacks/linked_stack.py
@@ -19,12 +19,25 @@ class Node:
 
 
 def push(stack, value):
+    # NOTE: Stacks are a special case of lists: they are lists that are only
+    #       accessible at one terminus. In a singly linked list, the beginning
+    #       can be accessed in O(1) time, whereas the end is accessed in O(n)
+    #       time, thus, the beginning ought to be the top-of-stack.
+    #
+    # Create a new node containing the given value.
+    # Set the new node's next to the head.
+    # Set the head to the new node.
+    # Increment the size.
     pass
 
 
 def pop(stack):
+    # Set the head to the head's next.
+    # Decrement the size.
+    # Return the "unlinked" node's value.
     pass
 
 
 def peek(stack):
+    # Return the head node's value.
     pass
