ab2e89aec81f655acc980c31f1e0feddf6243f68
Christopher Siu <christopher@siu.sh>
2024-07-17 10:37:18 -0700

    Added pseudocode for circular array queues.

--- 07.17-circular-queues/array_queue.py
+++ 07.17-circular-queues/array_queue.py
@@ -7,18 +7,39 @@ class Queue:
         # The backing array:
         self.array = [None] * self.capacity
         # The index of the front of the queue:
-        self.front = 0
+        self.front = -1
         # The index of the back of the queue:
-        self.back = 0
+        self.back = -1
 
 
 def enqueue(queue, value):
+    # NOTE: If front < 0 and back < 0, there are no elements in the queue.
+    #       If front <= back, the elements are the subarray [front:back + 1].
+    #       Else, they are the subarray [front:capacity + 1] + [0:back + 1].
+    #
+    # If the queue is at capacity, then:
+    #     Double the capactity.
+    #     Create a new array of that new capacity.
+    #     "Unwrap" the elements of the old array; copy them into the new array.
+    #     Set the backing array to the new array.
+    #
+    # If the queue is empty, then:
+    #     Set the front and back to 0.
+    # Else, do:
+    #     Increment the back and mod it by the capacity.
+    # Set the element in the array at the back index to the given value.
     pass
 
 
 def dequeue(queue):
+    # If the queue has one element, then:
+    #     Set the front and back to -1.
+    # Else, do:
+    #     Increment the front and mod it by the capacity.
+    # Return the element in the array at the old front index.
     pass
 
 
 def peek(queue):
+    # Return the element in the array at the front index.
     pass
