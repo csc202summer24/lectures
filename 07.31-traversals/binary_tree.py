f62e59a52ef5b543607daee641b59be328766fc5
Christopher Siu <christopher@siu.sh>
2024-07-31 09:58:08 -0700

    Added pseudocode for pre- and level-order traversals.

--- 07.31-traversals/binary_tree.py
+++ 07.31-traversals/binary_tree.py
@@ -52,8 +52,47 @@ def _traverse(node):
 
 
 def preorder(tree):
+    # NOTE: This could certainly be done iteratively, as with postorder, by
+    #       rearranging the recursive calls. Alternatively, any recursive
+    #       function can be made iterative by maintaining a "stack of jobs".
+    #
+    # Create a new, empty stack.
+    # Push the root onto the stack.
+    #
+    # While the stack is not empty, do:
+    #     Pop a node off of the stack.
+    #     Mark the node as traversed (do whatever we came here to do, e.g, put
+    #      the node into a queue).
+    #
+    #     NOTE: We want to traverse the left before the right, so the left must
+    #           be popped before the right, so the right must be pushed first.
+    #
+    #     If the node has a right child, then:
+    #         Push the right child onto the stack.
+    #     If the node has a left child, then:
+    #         Push the left child onto the stack.
+    #
+    # Return (whatever we were asked to return, e.g, a queue).
     pass
 
 
 def levelorder(tree):
+    # NOTE: Since stacks are LIFO, siblings are pushed before children, thus,
+    #       children are popped and traversed before siblings. If we instead
+    #       wish to traverse siblings first, we must instead use a FIFO queue.
+    #
+    # Create a new, empty queue.
+    # Enqueue the root onto the queue.
+    #
+    # While the queue is not empty, do:
+    #     Dequeue a node off of the queue.
+    #     Mark the node as traversed (do whatever we came here to do, e.g, put
+    #      the node into a queue -- NOT the "queue of jobs").
+    #
+    #     If the node has a left child, then:
+    #         Enqueue the left child onto the queue.
+    #     If the node has a right child, then:
+    #         Enqueue the right child onto the queue.
+    #
+    # Return (whatever we were asked to return, e.g, a queue -- NOT of jobs).
     pass
