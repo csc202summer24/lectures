e094f6238d65bf1448c2a0e5d605c7662035d686
Christopher Siu <christopher@siu.sh>
2024-08-05 10:02:30 -0700

    Added pseudocode for removing and traversing BSTs.

--- 08.05-bst-traversals/bst.py
+++ 08.05-bst-traversals/bst.py
@@ -59,8 +59,66 @@ def _insert(bst, node, key):
 
 
 def remove(bst, key):
+    # Set the root to the result of removing from the root.
     pass
 
 
+def _remove(bst, node, key):
+    # If the node does not exist, then:
+    #     Note that the key does not exist.
+    # Else if the node contains the given key, then:
+    #     If the node does not have a right child, then:
+    #         Decrement the size.
+    #         Return the left child.
+    #     Else if the node does not have a left child, then:
+    #         Decrement the size.
+    #         Return the right child.
+    #     Else, do:
+    #
+    #         NOTE: If the node has two children, we need to keep the node (the
+    #               only thing connecting those children), but we need to
+    #               replace its key (the thing actually being removed).
+    #               Consider the smallest key in the right subtree:
+    #                 * By definition, it's bigger than any to the left.
+    #                 * By construction, it's smaller than any to the left.
+    #               ...that smallest key to the right can take our place.
+    #
+    #         Find the smallest key in the right subtree.
+    #         Set the right child to the result of removing that smallest key
+    #          from the right child.
+    #         Set the node's key to that smallest key.
+    #         Return the given node.
+    #
+    # Else if the node's key is larger than the given key, then:
+    #     Set the left child to the result of recursing on that child.
+    #     Return the given node.
+    # Else, do:
+    #     Set the right child to the result of recursing on that child.
+    #     Return the given node.
+    pass
+
+
+def _min(node):
+    # If the node does not exist, then:
+    #     Note that there is no smallest key.
+    # Else if the node has no left child, then:
+    #     Return the node's key.
+    # Else, do:
+    #     Return the result of recursing on the left child.
+
+
 def inorder(bst):
+    # Return the result of traversing the tree's root.
+    pass
+
+
+def _traverse(node):
+    # If the node is None, then:
+    #     Return (whatever we were asked to return, e.g, a queue).
+    # Else, do:
+    #     Recurse on the node's left child.
+    #     Mark the node as traversed (do whatever we came here to do, e.g, put
+    #      the node into a queue).
+    #     Recurse on the node's right child.
+    #     Return (whatever we were asked to return, e.g, a queue).
     pass
