2b38cb7194a16bafc8c93a1bfd82724a74a827a7
Christopher Siu <christopher@siu.sh>
2024-08-12 10:02:04 -0700

    Added first attempt at pseudocode for dictionaries.

--- 08.12-dictionaries/dictionary.py
+++ 08.12-dictionaries/dictionary.py
@@ -10,27 +10,33 @@ class Dictionary:
         self.size = 0
 
 
-class Node:
-    """ A single node in a linked list """
-
-    def __init__(self, key, value, next):
-        # The key contained in this node:
-        self.key = key
-        # The value contained in this node:
-        self.value = value
-        # The next node in the linked list:
-        self.next = next
-
-
 def get(dct, key):
+    # Hash the given key and mod it by the capacity.
+    # Return that element in the backing array.
     pass
 
 
 def insert(dct, key, value):
+    # NOTE: The general idea is to convert keys back into array indices, as
+    #       arrays can be indexed in constant time. This only works as long
+    #       as we can guarantee that no two keys get converted into the same
+    #       index; we currently have no way of knowing otherwise if a value
+    #       is associated with the same key in the event of a collision...
+    #
+    # Hash the given key and mod it by the capacity.
+    # If that element in the backing array exists, then:
+    #     Set that element in the backing array to the given value.
+    # Else, do:
+    #     Set that element in the backing array to the given value.
+    #     Increment the size.
     pass
 
 
 def remove(dct, key):
+    # Hash the given key and mod it by the capacity.
+    # Unset that element in the backing array.
+    # Decrement the size.
+    # Return the "unset" element.
     pass
 
 
