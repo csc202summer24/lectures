72d9585fd50ed40e6966023710e66d8816dec34f
Christopher Siu <christopher@siu.sh>
2024-08-20 10:32:37 -0700

    Added base code for graphs.

---
+++ 08.21-graphs/graph.py
@@ -0,0 +1,23 @@
+import dictionary as dct
+
+
+class Graph:
+    """ A collection of vertices and edges """
+
+    def __init__(self):
+        # The backing adjacency matrix:
+        self.matrix = dct.Dictionary()
+        # The number of edges in this graph:
+        self.size = 0
+
+
+def add_vertex(graph, vertex):
+    pass
+
+
+def add_edge(graph, vertex_u, vertex_v):
+    pass
+
+
+def path(graph, vertex_u, vertex_v):
+    pass
